9b423cfa6573546c298984680f03a6969ae9aea6
Kaan İyice <kaaniyice35@gmail.com>
2023-12-18 16:30:21 +0300

    feat: add greet command

--- main.py
+++ main.py
@@ -9,13 +9,20 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
+
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} is loggen in.")
 
+
 @bot.command()
 async def hello(ctx):
     await ctx.send(f"Hello {bot.guilds[0]} server")
+
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(f"Hi, {ctx.message.author.global_name}")
 
 
 bot.run(BOT_TOKEN)
