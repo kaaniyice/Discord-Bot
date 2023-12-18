7540f4b4ba073a429619b029d4e6e59942d283cb
Kaan İyice <kaaniyice35@gmail.com>
2023-12-18 16:31:22 +0300

    feat: add direct message command

--- main.py
+++ main.py
@@ -25,4 +25,15 @@ async def greet(ctx):
     await ctx.send(f"Hi, {ctx.message.author.global_name}")
 
 
+# Command for checking direct message control of author
+@bot.command()
+async def directmessage(ctx):
+    try:
+        await ctx.message.author.send("Hello")
+    except discord.Forbidden:
+        print(f"Error sending DM to user : User has DMs disabled.")
+    except discord.HTTPException:
+        print(f"Error sending DM to user : Bot has no permission to send DMs.")
+
+
 bot.run(BOT_TOKEN)
