9959446940cfc0eeba393dbf0656d6511c28edbc
Kaan İyice <kaaniyice35@gmail.com>
2023-12-18 17:22:32 +0300

    feat: add spyfall game

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import random
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+SPYFALL_PLACES = os.getenv("SPYFALL_PLACES")
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
@@ -55,5 +56,34 @@ async def dice(ctx, num=6):
         number = random.randint(1, num)
         await ctx.send(f"Dice: {number}")
 
+@bot.command()
+async def spyfall(ctx, *who: discord.Member):
+    members = [i for i in who]
+    chosen_index = random.randint(0, len(members) - 1)
+    spy = members[chosen_index]
+    remaining_members = members[:chosen_index] + members[chosen_index + 1:]
+    with open(SPYFALL_PLACES, 'r') as f:
+        spyfall_places = f.readlines()
+        place = random.choice(spyfall_places)
+    for member in remaining_members:
+        user = discord.utils.get(bot.guilds[0].members, name=str(member))
+        if user:
+            try:
+                await user.send(f"You are ||**not the Spy**|| and the place is ||**{place}**||")
+            except discord.Forbidden:
+                print(f"Error sending DM to user {user.name}: User has DMs disabled.")
+            except discord.HTTPException:
+                print(f"Error sending DM to user {user.name}: Bot has no permission to send DMs.")
+            except:
+                print(f"There was an error sending message to {user.name}")
+    user = discord.utils.get(bot.guilds[0].members, name=str(spy))
+    try:
+        await user.send(f"You are ||**theeee Spy**|| and the place is ||**Unknown**||")
+    except discord.Forbidden:
+        print(f"Error sending DM to user {user.name}: User has DMs disabled.")
+    except discord.HTTPException:
+        print(f"Error sending DM to user {user.name}: Bot has no permission to send DMs.")
+    except:
+        print(f"There was an error sending message to {user.name}")
 
 bot.run(BOT_TOKEN)
